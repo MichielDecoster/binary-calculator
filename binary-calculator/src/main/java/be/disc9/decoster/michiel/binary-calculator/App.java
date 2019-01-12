21699f7a71fbe60f8cb256329c05170a352be531
Michiel@Laptop <decoster.michiel@gmail.com>
2019-01-12 14:09:28 +0100

    Made a maven project for the binary-calculator

---
+++ binary-calculator/src/main/java/be/disc9/decoster/michiel/binary-calculator/App.java
@@ -0,0 +1,17 @@
+package be.disc9.decoster.michiel.binary-calculator;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
